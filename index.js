dc0f8cd0f67167e2f751275bf3e3f2a91f19ac68
HayoungKim20 <hayoung0627@gmail.com>
2022-05-12 14:34:40 +0900

    connect DB

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+const { response } = require('express');
 var express = require('express')
 var app = express()
 app.listen(process.env.PORT || 3000, function () {
 	console.log("start! express server on port 3000");
 })
 
+const db = require('./db');
 app.get('/', function(req, res) {
 	res.send("Hello World")
+	var responseData = {};
+	var query = db.query('select * from board', (err, rows) => {
+		if (err) throw err;
+		if (rows.length) {
+			res.send('Success')
+		} else {
+			res.send('fail')
+		}
+	})
 })
