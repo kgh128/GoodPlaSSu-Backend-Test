1be936e567fbd144793928fd2a70d802a81beeda
kgh128 <hayoung0627@gmail.com>
2022-05-14 12:49:19 +0900

    Hide DB URI

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
+require('dotenv').config();
+
 var express = require('express')
 var app = express()
+
 app.listen(process.env.PORT || 3000, function () {
 	console.log("start! express server on port 3000");
 })
 
 const { Client } = require('pg');
-const connectionString = 'postgres://kxxbpytncaxnnz:cb3d3781392a05f804eea5299c0b5930e3f4dcfb0bfd1e289993e3110f61eb7b@ec2-3-217-113-25.compute-1.amazonaws.com:5432/d2rkvlm7s19est';
+const connectionString = process.env.DB_URI;
 
 const client = new Client({
 	connectionString,
-})
+	ssl: {
+		rejectUnauthorized: false,
+	}
+});
 client.connect(err => {
 	if (err) console.log(err);
 	else {
